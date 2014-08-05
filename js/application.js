2cd16c2612a450720db41ce74ec2c1b002d5fa1f
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-08-04 21:50:19 -0300

    fix phrase

--- js/application.js
+++ js/application.js
@@ -22,7 +22,7 @@ var App = {
                                 <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 col-xs-offset-0 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">\
                                     <div class="single_post_content">\
                                         <div class="sinle_blockquote">\
-                                            <h2><a href="#"><sup>&ldquo;</sup>' + $phrase + '<sup>&rdquo;</sup></a></h2>\
+                                            <h2><sup>&ldquo;</sup>' + $phrase + '<sup>&rdquo;</sup></h2>\
                                         </div>\
                                     </div>\
                                 </div>\
