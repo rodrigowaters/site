547bd247f645d6ed291c3a626eb4503c1e607d6e
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-08-05 23:49:56 -0300

    parcial

--- js/application.js
+++ js/application.js
@@ -68,7 +68,9 @@ var App = {
         });
         App.get_json();
         $('#modal').on('hidden.bs.modal', function(e) {
+            var $hash = window.location.hash;
             window.location.hash = '';
+            window.location.hash = $hash;
         });
     }
 };
